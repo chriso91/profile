cca4c550b059f6569e84bfdc6bddb0777364a728
chriso91 <c.e.odea@gmail.com>
2021-11-02 13:08:57 -0400

    chore: add jpg loader in prod

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -38,6 +38,14 @@ const config: Configuration = {
            { loader: "sass-loader" },
          ],
        },
+       {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+       },
     ],
   },
   resolve: {
